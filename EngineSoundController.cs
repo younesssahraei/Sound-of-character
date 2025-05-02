63111186844c1584d0603c76d351b6d982140d37
younesssahraei <127088769+younesssahraei@users.noreply.github.com>
2025-05-02 16:22:38 +0330

    Add files via upload

---
+++ EngineSoundController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class EngineSoundController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float pitchMultiplier = 0.05f;
+    private Rigidbody rb;
+    private AudioSource engineAudio;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        engineAudio = GetComponent<AudioSource>();
+        engineAudio.loop = true;
+        engineAudio.Play();
+    }
+
+    void Update()
+    {
+        // حرکت کاراکتر
+        float moveInput = Input.GetAxis("Vertical");
+        Vector3 move = transform.forward * moveInput * speed;
+        rb.velocity = move;
+
+        // تنظیم pitch صدا بر اساس سرعت
+        float currentSpeed = rb.velocity.magnitude;
+        engineAudio.pitch = 1.0f + currentSpeed * pitchMultiplier;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))] // اگر کاراکتر 2D هست
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    void Update()
+    {
+        // گرفتن ورودی کاربر
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // مقداردهی انیمیشن
+        float speed = movement.sqrMagnitude;
+        animator.SetFloat("Speed", speed);
+    }
+
+    void FixedUpdate()
+    {
+        // حرکت دادن فیزیکی کاراکتر
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
